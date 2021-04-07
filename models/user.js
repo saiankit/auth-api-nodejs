16bf58e1680a1d35803900e7ab917a8cbf458d3f
Sai Ankit <saiankit30@gmail.com>
2021-04-07 13:57:56 +0530

    Add User Model
    Add User Model which is a schema for the User Object to be stored on MongoDB

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
